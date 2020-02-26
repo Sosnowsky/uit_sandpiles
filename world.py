1da20ff5059124fd7b3fffa247c29d5004368f3e
AFNordal <nordalrasmus01@gmail.com>
2020-02-26 11:45:13 +0100

    Comments and progress bar

--- world.py
+++ world.py
@@ -2,6 +2,7 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from tqdm import tqdm
 
 
 class World:
@@ -20,24 +21,27 @@ class World:
 
 		self.grains = 0
 		self.plane = np.array(None)
-
 		self.persistent_diff = np.zeros((self.ROWS, self.COLS), dtype=int)
+
+		self.stats = {
+                    'crits': []
+		}
 
 		self.init_plane()  # Initiate the plane randomly or from file
 
 		# Write header to data file
 		with open(self.OUTPUT, 'a+') as data_file:
 			data_file.write(
-				'========================================================\n')
+                            '========================================================\n')
 			data_file.write(
-				'critical cells; added grains; lost grains; total grains;\n')
+                            'critical cells; added grains; lost grains; total grains;\n')
 			data_file.write(
-				'========================================================\n')
+                            '========================================================\n')
 
 	def init_plane(self):
 		if self.INPUT == '':  # initiate plane randomly
 			self.plane = np.fromfunction(np.vectorize(lambda r, c: random.randint(
-				0, 3)), (self.ROWS, self.COLS), dtype=int)
+                            0, 3)), (self.ROWS, self.COLS), dtype=int)
 
 		else:  # Initiate plane from file
 			self.plane = np.empty((self.ROWS, self.COLS), dtype=int)
@@ -48,6 +52,7 @@ class World:
 		# Calculate the number of grains
 		self.grains = sum(self.plane.flatten())
 
+	# Runs one timestep
 	def step(self, p_diff, p_crits):
 
 		lost = 0
@@ -61,6 +66,7 @@ class World:
 		crits = 0
 		self.grains += added
 
+		# Function to place grain on adjacent cells
 		def put(r, c):
 			nonlocal lost
 			try:
@@ -94,36 +100,45 @@ class World:
 		return None
 		# /BOUNDARY CONDITION
 
+	# Returns random position to place new grain on
 	def rand_pos(self):
 		r = random.randint(0, self.ROWS - 1)
 		c = random.randint(0, self.COLS - 1)
 		return r, c
 
 	# Runs the simulation for n timesteps and saves data
-
 	def drive(self, n, verbose=1, animate=False):
 		if animate:
 			self.reset_animation()
 		with open(self.OUTPUT, 'a+') as data_file:
 			diff = self.persistent_diff
 			crits = 0
-			for i in range(n):
+			rng = range(n)
+			if verbose == 2:
+				rng = tqdm(rng, ncols=100)
+
+			for i in rng:
 				crits, added, lost, diff = self.step(diff, crits)
 				data_file.write('{};{};{};{};\n'.format(crits, added, lost, self.grains))
+				self.stats['crits'].append(crits)
 				if animate:
 					self.add_frame()
 				if verbose == 1:
-					print('\r{}/{}'.format(i, n), end='')
-				elif verbose == 2:
+					print('\r{}/{}'.format(i + 1, n), end='')
+					if i == n - 1:
+						print('')
+				elif verbose == 3:
 					print(self.draw() + '\n')
 			self.persistent_diff = diff
-			if verbose == 1:
-				print('\r{}/{}'.format(n, n))
-		if animate:
-			self.show_animation()
+
 		if self.SAVE != '':
 			with open(self.SAVE, 'w+') as save_file:
 				save_file.write(self.draw())
+		if animate:
+			self.show_animation()
+		else:
+			plt.plot(self.stats['crits'])
+			plt.show()
 
 	# Returns a map of the world plane
 	def draw(self):
@@ -141,12 +156,12 @@ class World:
 
 	def show_animation(self):
 		self.im = plt.imshow(
-			self.frames[0], animated=True, cmap='jet', vmax=4, vmin=0)
+                    self.frames[0], animated=True, cmap='jet', vmax=5, vmin=0)
 
 		ani = animation.FuncAnimation(self.canvas, self.get_frame, frames=range(
-			len(self.frames)), interval=10, blit=True, repeat=True, repeat_delay=1000)
+                    len(self.frames)), interval=10, blit=True, repeat=True, repeat_delay=1000)
+		# ani.save('data/out_ani.mp4', bitrate=30000)   # uncomment this to save animation
 		plt.show()
-		ani.save('data/out_ani.mp4')
 		self.reset_animation()
 
 	def get_frame(self, i):
