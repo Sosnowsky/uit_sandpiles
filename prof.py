1629b4edeaee76ff1525cdbc49109ccc2bcf429b
Rasmus Anker Fossen Nordal <rno040@nobs-ntf-m0022.bargi.uit.no>
2020-07-28 16:52:12 +0200

    added profiling

---
+++ prof.py
@@ -0,0 +1,14 @@
+import pstats
+import cProfile
+from yaml import safe_load
+from world import World
+
+with open("config.yml", "r") as cfg:
+	config = safe_load(cfg)
+
+world = World(config)
+
+cProfile.runctx("world.drive(100, 0, 0, 0)", globals(), locals(), "Profile.prof")
+
+s = pstats.Stats("Profile.prof")
+s.strip_dirs().sort_stats("time").print_stats()
