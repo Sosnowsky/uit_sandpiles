fda2435a6b3347f44bf7757dca150d5d4ba9374c
Rasmus Anker Fossen Nordal <rno040@nobs-ntf-m0022.bargi.uit.no>
2020-07-30 14:49:43 +0200

    ranges of probabilities

--- sandpiles.py
+++ sandpiles.py
@@ -1,10 +1,12 @@
 from yaml import safe_load
 from world import World
 from subprocess import call
+from tqdm import tqdm
 import signal
+from copy import deepcopy
 
 reps = 500
-sets = 200
+offsite = False
 
 local_backup = "backups"
 network_backup = "rasmus@bringebaerpai.duckdns.org:~/backups"
@@ -13,16 +15,30 @@ with open("config.yml", "r") as cfg:
 	config = safe_load(cfg)
 
 call(["cp", "config.yml", local_backup + "/"])
-call(["scp", "-P", "2200", "config.yml", network_backup + "/"])
+if offsite:
+	call(["scp", "-P", "2200", "config.yml", network_backup + "/"])
 
-world = World(config)
+worlds = []
+configs = []
+p0 = config["probability"]
+dp = config["d_probability"]
+n_worlds = config["n_runs"]
+for i in range(n_worlds):
+	mod_config = deepcopy(config)
+	p = p0 + i * dp
+	mod_config["probability"] = p
+	mod_config["output"]["data"] = config["output"]["data"].replace("/", f"/{i}/")
+	mod_config["output"]["map"] = config["output"]["map"].replace("/", f"/{i}/")
+	worlds.append(World(mod_config))
+	configs.append(mod_config)
 
 
 if config["input"] == "":
 	print(
-		"No input file specified - initiating randomly and driving to a critical configuration"
+		"No input file specified - initiating randomly and driving to critical configurations"
 	)
-	world.drive_to_stable()
+	for world in worlds:
+		world.drive_to_stable()
 
 
 class Flag:
@@ -31,28 +47,36 @@ class Flag:
 		signal.signal(signal.SIGINT, self.change_state)
 
 	def change_state(self, signum, frame):
-		print("Exit flag set to True (repeat to exit now)")
+		progress_print(string="Exit flag set to True (repeat to exit now)")
 		signal.signal(signal.SIGINT, signal.SIG_DFL)
 		self.flag = True
 
 
+def progress_print(s=None, r=None, lines_up=1, string=None):
+	if string:
+		tqdm.write("\033[F" * lines_up + "\033[K" + string)
+	else:
+		tqdm.write(
+			"\033[F" * lines_up + f"\033[K{s} sets of {r} timesteps simulated in each world"
+		)
+
+
 c = 0
 flag = Flag()
+print()
 while True:
-	for i in range(sets):
-		world.drive(reps, verbose=2, animate=False, graph=False)
-		print(str(i + 1) + f"/{sets}")
-		if flag.flag:
-			print("Exiting")
+	for i, world in enumerate(tqdm(worlds, leave=False)):
+		world.drive(reps, verbose=2, animate=False, graph=False, nest_tqdm=True)
+		if flag.flag or i == n_worlds - 1:
+			progress_print(c + int(not flag.flag), reps)
 			break
-
-	print("Creating backup")
-	call(["cp", config["output"]["data"], "{}/{}.d".format(local_backup, c % 10)])
-	call(["cp", config["output"]["map"], "{}/{}.m".format(local_backup, c % 10)])
-	call(
-		"scp -P 2200 {}/{}.* {}/".format(local_backup, c % 10, network_backup), shell=True
-	)
 	if flag.flag:
 		break
+
+	for i, conf in enumerate(configs):
+		call(["cp", conf["output"]["data"], "{}/{}.d".format(local_backup, i)])
+		call(["cp", conf["output"]["map"], "{}/{}.m".format(local_backup, i)])
+		if offsite:
+			call("scp -P 2200 {}/{}.* {}/".format(local_backup, i, network_backup), shell=True)
 
 	c += 1
