bacddc35ebf30eb9220de506ffd12d63e73afb58
Rasmus Anker Fossen Nordal <rno040@nobs-ntf-m0022.bargi.uit.no>
2020-08-07 16:30:19 +0200

    blups

--- analysis.py
+++ analysis.py
@@ -1,29 +1,30 @@
 from tqdm import tqdm
 from scipy.signal import welch
 
+for i in range(20):
 
-dur = 0
-area = 0
+	dur = 0
+	area = 0
 
-crits = []
+	crits = []
 
-with open("./data/p0.0001_256/data.txt", "r") as raw, open(
-	"./data/p0.0001_256/events.txt", "w"
-) as out:
-	for line in tqdm(raw.readlines()[3:]):
-		strs = line.rstrip(";\n\r ").split(";")
-		crit = int(strs[0])
-		crits.append(crit)
-		if crit == 0 and dur > 0:
-			out.write(f"{dur};{area}\n")
-			dur = 0
-			area = 0
-		elif crit > 0:
-			dur += 1
-			area += crit
+	with open(f"./data/prange_5e-5/{i}.d", "r") as raw, open(
+		f"./data/prange_5e-5/{i}.e", "w"
+	) as out:
+		for line in tqdm(raw.readlines()[3:]):
+			strs = line.rstrip(";\n\r ").split(";")
+			crit = int(strs[0])
+			crits.append(crit)
+			if crit == 0 and dur > 0:
+				out.write(f"{dur};{area}\n")
+				dur = 0
+				area = 0
+			elif crit > 0:
+				dur += 1
+				area += crit
 
-N = len(crits)
-freq, density = welch(crits, nperseg=N / 10000)
-with open("./data/p0.0001_256/nps10000.txt", "w") as file:
-	for f, s in zip(freq, density):
-		file.write(f"{f};{s}\n")
+	N = len(crits)
+	freq, density = welch(crits, nperseg=N / 10000)
+	with open(f"./data/prange_5e-5/{i}.f", "w") as file:
+		for f, s in zip(freq, density):
+			file.write(f"{f};{s}\n")
