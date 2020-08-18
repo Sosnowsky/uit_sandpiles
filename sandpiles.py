db38c7fa6e2695b3fbc60c007ce23a1a83b96d63
Rasmus Anker Fossen Nordal <rno040@nobs-ntf-m0022.bargi.uit.no>
2020-08-18 10:49:08 +0200

    Fixed sandpiles.py to work with py3.8

--- sandpiles.py
+++ sandpiles.py
@@ -10,74 +10,11 @@ import sys
 from tqdm import tqdm
 from itertools import count
 
-
-def rreplace(s, r1, r2):
-	# Convenience function to replace last instance of substring
-	return r2.join(s.rsplit(r1, 1))
-
-
 # cython uses the rest of the cores more efficiently than multiprocessing can
 cores_used = cpu_count() // 2
 
 # Number of timesteps to run in each world per batch
 reps = 500
-
-# Directory to store backups
-local_backup = "backups"
-
-# Load config file
-with open("config.yml", "r") as cfg:
-	config = safe_load(cfg)
-
-# Back up config file
-call(["cp", "config.yml", local_backup + "/"])
-
-# Lists to store worlds and their config dicts
-worlds = []
-configs = []
-
-# Probability used in world 0
-p0 = config["probability"]
-
-# Probability increment
-dp = config["d_probability"]
-n_worlds = config["n_runs"]
-
-# Initiate each world
-for i in range(n_worlds):
-	# Calculate probability to use in this world
-	p = p0 + i * dp
-
-	# Copy master config dict and modify it
-	mod_config = deepcopy(config)
-	mod_config["probability"] = p
-	mod_config["seed"] = config["seed"] + i
-	mod_config["output"]["data"] = rreplace(config["output"]["data"], "/", f"/{i}_")
-	mod_config["output"]["map"] = rreplace(config["output"]["map"], "/", f"/{i}_")
-	# mod_config["input"] = rreplace(config["input"], "/", f"/{i}_") # Uncomment this if the input files are individual for each world
-	worlds.append(World(mod_config))
-	configs.append(mod_config)
-
-# If there is no input file supplied, each world is run to a stable configuration
-if config["input"] == "":
-	print(
-		"No input file specified - initiating randomly and driving to critical configurations"
-	)
-
-	# Wrapper function for multiprocessing
-	def drive_to_stable_wrapper(tup):
-		i, world = tup
-		sys.stdout = open(f"proclogs/world{i}.out", "a", buffering=2)
-		sys.stderr = open(f"proclogs/world{i}_err.out", "a", buffering=2)
-		World.drive_to_stable(world)
-
-	# Create process pool and run until all worlds are stable
-	pool = Pool(cores_used)
-	for res in tqdm(pool.imap_unordered(drive_to_stable_wrapper, enumerate(worlds))):
-		pass
-	pool.close()
-	pool.join()
-	print("Criticality reached in all systems")
 
 
 class Flag:
@@ -96,41 +33,115 @@ class Flag:
 		self.flag = True
 
 
+flag = Flag()
+
+
+# Wrapper function for multiprocessing
+def drive_to_stable_wrapper(tup):
+	i, world = tup
+	sys.stdout = open(f"proclogs/world{i}.out", "a", buffering=2)
+	sys.stderr = open(f"proclogs/world{i}_err.out", "a", buffering=2)
+	World.drive_to_stable(world)
+
+
 # Wrapper function for multiprocessing
 def drive_wrapper(tup):
 	i, world = tup
 	if i % cores_used:
 		sys.stdout = open(f"proclogs/world{i}.out", "a", buffering=2)
 		sys.stderr = open(f"proclogs/world{i}_err.out", "a", buffering=2)
-		World.drive(world, reps, verbose=0)
+		try:
+			World.drive(world, reps, verbose=0)
+		except KeyboardInterrupt:
+			exit()
 	else:  # This block is only run by one process at a time, as it prints to stdout
 		sys.stdout = sys.__stdout__
 		sys.stderr = sys.__stderr__
-		World.drive(world, reps, verbose=2, nest_tqdm=True)
+		try:
+			World.drive(world, reps, verbose=2, nest_tqdm=True)
+		except KeyboardInterrupt:
+			exit()
 
 
-flag = Flag()
-try:
-	for c in count():  # Infinite range
-		# Create a new pool each batch and run 'reps' timesteps in each world
+if __name__ == "__main__":
+
+	def rreplace(s, r1, r2):
+		# Convenience function to replace last instance of substring
+		return r2.join(s.rsplit(r1, 1))
+
+	# Directory to store backups
+	local_backup = "backups"
+
+	# Load config file
+	with open("config.yml", "r") as cfg:
+		config = safe_load(cfg)
+
+	# Back up config file
+	call(["cp", "config.yml", local_backup + "/"])
+
+	# Lists to store worlds and their config dicts
+	worlds = []
+	configs = []
+
+	# Probability used in world 0
+	p0 = config["probability"]
+
+	# Probability increment
+	dp = config["d_probability"]
+	n_worlds = config["n_runs"]
+
+	# Initiate each world
+	for i in range(n_worlds):
+		# Calculate probability to use in this world
+		p = p0 + i * dp
+
+		# Copy master config dict and modify it
+		mod_config = deepcopy(config)
+		mod_config["probability"] = p
+		mod_config["seed"] = config["seed"] + i
+		mod_config["output"]["data"] = rreplace(config["output"]["data"], "/", f"/{i}_")
+		mod_config["output"]["map"] = rreplace(config["output"]["map"], "/", f"/{i}_")
+		mod_config["input"] = rreplace(
+			config["input"], "/", f"/{i}_"
+		)  # Uncomment this if the input files are individual for each world
+		worlds.append(World(mod_config))
+		configs.append(mod_config)
+
+	# If there is no input file supplied, each world is run to a stable configuration
+	if config["input"] == "":
+		print(
+			"No input file specified - initiating randomly and driving to critical configurations"
+		)
+
+		# Create process pool and run until all worlds are stable
 		pool = Pool(cores_used)
-		for res in tqdm(
-			pool.imap_unordered(drive_wrapper, enumerate(worlds)),
-			position=0,
-			desc=f"Batch {c}",
-			total=n_worlds,
-		):
+		for res in tqdm(pool.imap_unordered(drive_to_stable_wrapper, enumerate(worlds))):
 			pass
 		pool.close()
 		pool.join()
+		print("Criticality reached in all systems")
 
-		# Back up data and map files
-		for i, conf in enumerate(configs):
-			call(["cp", conf["output"]["data"], "{}/{}.d".format(local_backup, i)])
-			call(["cp", conf["output"]["map"], "{}/{}.m".format(local_backup, i)])
+	try:
+		for c in count():  # Infinite range
+			# Create a new pool each batch and run 'reps' timesteps in each world
+			pool = Pool(cores_used)
+			for res in tqdm(
+				pool.imap_unordered(drive_wrapper, enumerate(worlds)),
+				position=0,
+				desc=f"Batch {c}",
+				total=n_worlds,
+			):
+				pass
+			pool.close()
+			pool.join()
 
-		if flag.flag:
-			break
+			# Back up data and map files
+			for i, conf in enumerate(configs):
+				call(["cp", conf["output"]["data"], "{}/{}.d".format(local_backup, i)])
+				call(["cp", conf["output"]["map"], "{}/{}.m".format(local_backup, i)])
 
-except KeyboardInterrupt:
-	pool.terminate()  # Kill all processes
+			if flag.flag:
+				break
+
+	except KeyboardInterrupt:
+		pool.terminate()  # Kill all processes
