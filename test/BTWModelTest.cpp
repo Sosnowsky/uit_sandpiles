8414160971811cb2b5b920a76a44fea518fab71a
Sosnowsky <juan.manuel.losada.sosnovsky@gmail.com>
2020-11-04 13:49:24 +0100

    Update .gitignore

---
+++ test/BTWModelTest.cpp
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+#include "../src/BTWModel.h"
+
+class BTWModelTest : public ::testing::Test {
+ protected:
+  BTWModelTest() = default;
+  ~BTWModelTest() override = default;
+ public:
+  BTWModel model = BTWModel("", "", 50);
+};
+
+TEST_F(BTWModelTest, ConvertsCharToUpperCaseString) {
+  std::cout <<" lol " << std::endl;
+  ASSERT_EQ(model.GetCriticalSites(), 50);
+}
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
